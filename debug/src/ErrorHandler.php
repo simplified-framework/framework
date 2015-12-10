ea799000335a2191ded33fe0fe42281ec440b7d0
Andreas Bratfisch <duskman72@gmail.com>
2015-12-10 07:23:55 +0100

    last changes

--- debug/src/ErrorHandler.php
+++ debug/src/ErrorHandler.php
@@ -303,19 +303,8 @@ class ErrorHandler {
     
     private static function compressCss() {
     	$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', self::$css);
-    	// Remove space after colons
     	$buffer = str_replace(': ', ':', $buffer);
-    	// Remove whitespace
     	$buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
-    	// Enable GZip encoding.
-    	//ob_start("ob_gzhandler");
-    	// Enable caching
-    	//header('Cache-Control: public');
-    	// Expire in one day
-    	//header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT');
-    	// Set the correct MIME type, because Apache won't set it for us
-    	//header("Content-type: text/css");
-    	// Write everything out
     	return $buffer;
     }
 }
