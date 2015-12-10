d96ac75e48493565b8911ed00ac33cb3d98b917d
Andreas Bratfisch <duskman72@gmail.com>
2015-12-10 19:06:45 +0100

    implement fallback

--- core/src/Lang.php
+++ core/src/Lang.php
@@ -4,7 +4,9 @@ namespace Simplified\Core;
 use Simplified\Config\PHPFileLoader;
 
 class Lang {
-    private static $i18n;
+    private static $language;
+    private static $fallback;
+    
 	private function __construct() {
         $loader = new PHPFileLoader();
         $params = $loader->load(CONFIG_PATH . 'language.php', array());
@@ -13,17 +15,23 @@ class Lang {
         if ($language == null) {
             $language = isset($params['default']) ? $params['default'] : null;
         }
+        
+        $fallback = isset($params['default']) ? $params['default'] : null;
 
         if ($language == null)
             throw new LanguageException('No language variable configured.');
+        
+        if ($fallback == null)
+            throw new LanguageException('No default language variable configured.');
 
-        self::$i18n = I18N_PATH . $language . DIRECTORY_SEPARATOR;
+        self::$language = I18N_PATH . $language . DIRECTORY_SEPARATOR;
+        self::$fallback = I18N_PATH . $fallback . DIRECTORY_SEPARATOR;
 	}
 	
 	// $key schema = file.lang_variable
 	// $values can contain replaces in syntax :name
 	public static function get($key, array $values = array()) {
-        if (self::$i18n == null) {
+        if (self::$language == null) {
             new self();
         }
 
@@ -31,15 +39,18 @@ class Lang {
             throw new LanguageException('No namespace in translation value found.');
 
         $parts = explode(".", $key);
-        $lang_filepath = self::$i18n . $parts[0] . ".php";
+        $filepath = self::$language . $parts[0] . ".php";
 
-        if (!file_exists($lang_filepath)) {
-        	throw new LanguageException('Unable to open translation file at ' . $lang_filepath);
+        if (!file_exists($filepath)) {
+        	$filepath = self::$fallback . $parts[0] . ".php";
+        	if (!file_exists($filepath)) {
+        		throw new LanguageException('Unable to open translation file at ' . $filepath);
+        	}
         }
 
 		// load language file
 		$loader = new PHPFileLoader();
-        $translations = $loader->load($lang_filepath, array());
+        $translations = $loader->load($filepath, array());
         
         if (!isset($translations[$parts[1]])) {
         	throw new LanguageException('Unable to find translation for ' . $parts[1]);
