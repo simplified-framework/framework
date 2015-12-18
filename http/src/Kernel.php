4f6a0f6b1d952402a79bb716e98fba5a08229e1b
Andreas Bratfisch <duskman72@gmail.com>
2015-12-18 18:58:22 +0100

    move defines to kernel

--- http/src/Kernel.php
+++ http/src/Kernel.php
@@ -1,5 +1,15 @@
 <?php
 
+define ("BASE_PATH",   dirname(dirname($_SERVER['SCRIPT_FILENAME'])) . DIRECTORY_SEPARATOR);
+define ("VENDOR_PATH", BASE_PATH . "vendor" . DIRECTORY_SEPARATOR);
+define ("PUBLIC_PATH", BASE_PATH . "public" . DIRECTORY_SEPARATOR);
+define ("APP_PATH", BASE_PATH . "app" . DIRECTORY_SEPARATOR);
+define ("STORAGE_PATH", APP_PATH . "storage" . DIRECTORY_SEPARATOR);
+define ("I18N_PATH", APP_PATH . "i18n" . DIRECTORY_SEPARATOR);
+define ("RESOURCES_PATH", APP_PATH . "resources" . DIRECTORY_SEPARATOR);
+define ("RESOURCES_VENDOR_PATH", RESOURCES_PATH . "vendor" . DIRECTORY_SEPARATOR);
+define ("CONFIG_PATH", APP_PATH . "config" . DIRECTORY_SEPARATOR);
+
 namespace Simplified\Http;
 
 use Simplified\Core\IllegalArgumentException;
