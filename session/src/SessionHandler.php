508c00496dabfab7b634700cb4a2a8cc7e772d0e
Andreas Bratfisch <duskman72@gmail.com>
2015-12-18 20:04:34 +0100

    add session handlerr

--- session/src/SessionHandler.php
+++ session/src/SessionHandler.php
@@ -10,16 +10,29 @@ namespace Simplified\Session;
 
 
 class SessionHandler implements \SessionHandlerInterface {
-    public function read($session_id) {
+    private $sessionpath;
+    public function __construct() {
+        $this->sessionpath = session_save_path();
+    }
 
+    public function read($session_id) {
+        if (file_exists($this->sessionpath . DIRECTORY_SEPARATOR . $session_id))
+            return file_get_contents($this->sessionpath . DIRECTORY_SEPARATOR . $session_id);
+
+        return "";
     }
 
     public function write($session_id, $data) {
+        $file = $this->sessionpath . DIRECTORY_SEPARATOR . $session_id;
+        $fp = fopen($file, "w");
+        fwrite($fp, $data);
+        fclose($fp);
 
+        return true;
     }
 
     public function open($session_path, $session_id) {
-
+        return true;
     }
 
     public function close() {
@@ -27,10 +40,12 @@ class SessionHandler implements \SessionHandlerInterface {
     }
 
     public function gc($max_life_time) {
-
+        return true;
     }
 
     public function destroy($session_id) {
-
+        if (file_exists($this->sessionpath . DIRECTORY_SEPARATOR . $session_id))
+            @unlink($this->sessionpath . DIRECTORY_SEPARATOR . $session_id);
+        return true;
     }
 }
