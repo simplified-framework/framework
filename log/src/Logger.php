631717adb6006cdaa1fe3465c803e1de5293ef12
Andreas Bratfisch <duskman72@gmail.com>
2015-12-19 11:15:14 +0100

    Logger: fix bugs, remove possible log spam

--- log/src/Logger.php
+++ log/src/Logger.php
@@ -13,16 +13,42 @@ use Psr\Log\LogLevel;
 class Logger extends AbstractLogger {
     private static $logdir;
 
+    public function __construct() {
+    }
+
     public function log($level, $message, array $context = array()) {
         if ($this->ensureLogDir()) {
-            $fp = fopen(self::$logdir . DIRECTORY_SEPARATOR . $level . ".txt", "a");
-            if ($fp) {
+            $file = self::$logdir . DIRECTORY_SEPARATOR . $level . ".txt";
+            $lastLine = $this->getLastFileLine($file);
+            if (strstr($lastLine, $message) !== false) {
+                return;
+            }
+
+            if (($fp = fopen($file, "a"))) {
                 $line = (new \DateTime())->format('Y-m-d H:i:s');
-                $line .= " " . trim($message) . "\n";
+                $line .= " " . trim($message) . PHP_EOL;
                 fwrite($fp, $line);
                 fclose($fp);
             }
         }
+    }
+
+    private function getLastFileLine($file) {
+        if (!file_exists($file))
+            return "";
+
+        $content = file_get_contents($file);
+        $lines = explode(PHP_EOL, $content);
+        $last = "";
+
+        for ($i = 0; $i < count($lines); $i++) {
+            if ($i == count($lines)-2) {
+                $last = $lines[$i];
+                break;
+            }
+        }
+
+        return $last;
     }
 
     private function ensureLogDir() {
