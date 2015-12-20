ebb185db3b12656c00b0c1480b4d638d7c9102d1
Andreas Bratfisch <duskman72@gmail.com>
2015-12-20 22:53:25 +0100

    extend command class

--- console/src/Migrate.php
+++ console/src/Migrate.php
@@ -12,16 +12,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-define ("BASE_PATH",   getcwd() . DIRECTORY_SEPARATOR);
-define ("VENDOR_PATH", BASE_PATH . "vendor" . DIRECTORY_SEPARATOR);
-define ("PUBLIC_PATH", BASE_PATH . "public" . DIRECTORY_SEPARATOR);
-define ("APP_PATH", BASE_PATH . "app" . DIRECTORY_SEPARATOR);
-define ("STORAGE_PATH", APP_PATH . "storage" . DIRECTORY_SEPARATOR);
-define ("I18N_PATH", APP_PATH . "i18n" . DIRECTORY_SEPARATOR);
-define ("RESOURCES_PATH", APP_PATH . "resources" . DIRECTORY_SEPARATOR);
-define ("RESOURCES_VENDOR_PATH", RESOURCES_PATH . "vendor" . DIRECTORY_SEPARATOR);
-define ("CONFIG_PATH", APP_PATH . "config" . DIRECTORY_SEPARATOR);
-
 class Migrate extends Command {
     protected function configure() {
         $this
--- console/src/MigrateMake.php
+++ console/src/MigrateMake.php
@@ -13,16 +13,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Simplified\Config\Config;
 
-define ("BASE_PATH",   getcwd() . DIRECTORY_SEPARATOR);
-define ("VENDOR_PATH", BASE_PATH . "vendor" . DIRECTORY_SEPARATOR);
-define ("PUBLIC_PATH", BASE_PATH . "public" . DIRECTORY_SEPARATOR);
-define ("APP_PATH", BASE_PATH . "app" . DIRECTORY_SEPARATOR);
-define ("STORAGE_PATH", APP_PATH . "storage" . DIRECTORY_SEPARATOR);
-define ("I18N_PATH", APP_PATH . "i18n" . DIRECTORY_SEPARATOR);
-define ("RESOURCES_PATH", APP_PATH . "resources" . DIRECTORY_SEPARATOR);
-define ("RESOURCES_VENDOR_PATH", RESOURCES_PATH . "vendor" . DIRECTORY_SEPARATOR);
-define ("CONFIG_PATH", APP_PATH . "config" . DIRECTORY_SEPARATOR);
-
 class MigrateMake extends Command {
     protected function configure() {
         $this
