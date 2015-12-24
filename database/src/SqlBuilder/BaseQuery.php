b0b87d3c7638727d902182a6e2a5bfc13d6a3e12
Andreas Bratfisch <duskman72@gmail.com>
2015-12-24 07:44:02 +0100

    reimplement SqlBuilder

--- database/src/SqlBuilder/BaseQuery.php
+++ database/src/SqlBuilder/BaseQuery.php
@@ -17,13 +17,29 @@ class BaseQuery {
         switch (func_num_args()) {
             case 1:
                 // support raw where clauses: where("field = value")
+                // support Closure where clauses: where(function($query){})
                 $this->andWhere[] = func_get_arg(0);
                 break;
             case 2:
                 // support short hand where clause: where("field" = "value")
+                // support short hand whereIN clause: where("field", "IN",  array())
                 break;
             case 3:
-                // support where clause with operator: where("field", "<,>,=,!=,NOT,IN", "value")
+                // support where clause with operator: where("field", "<,>,=,!=,NOT,IN,NOT IN", "value")
+                // validate function args
+                if (WhereOperator::isValid(func_get_arg(1)))
+                    throw new IllegalArgumentException("Operator must on of '<,>,=,!=,NOT,IN,NOT IN'");
+
+                if (!is_string(func_get_arg(0)) || !is_string(func_get_arg(1)))
+                    throw new IllegalArgumentException("First and second argument must be string");
+
+                if (is_array(func_get_arg(2)) && (func_get_arg(1) != WhereOperator::IN || func_get_arg(1) != WhereOperator::NOT_IN))
+                    throw new IllegalArgumentException("Second argument can't be used to compare table field and array");
+
+                if ((func_get_arg(1) == WhereOperator::IN || func_get_arg(1) == WhereOperator::NOT_IN) && !is_array(func_get_arg(2)))
+                    throw new IllegalArgumentException("Third argument must be array");
+
+
                 break;
             case 0:
                 throw new IllegalArgumentException("Where clause needs at least one argument");
---
+++ database/src/SqlBuilder/WhereOperator.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andreas
+ * Date: 24.12.2015
+ * Time: 07:20
+ */
+
+namespace Simplified\Database\SqlBuilder;
+
+
+class WhereOperator {
+    const LESS_THAN = '<';
+    const GREATER_THAN = '>';
+    const EQUAL = '=';
+    const NOT_EQUAL = '!=';
+    const NOT = 'NOT';
+    const IN = 'IN';
+    const NOT_IN = 'NOT IN';
+
+    public static function isValid($op) {
+        switch ($op) {
+            case WhereOperator::LESS_THAN:
+            case WhereOperator::GREATER_THAN:
+            case WhereOperator::EQUAL:
+            case WhereOperator::NOT_EQUAL:
+            case WhereOperator::NOT:
+            case WhereOperator::IN:
+            case WhereOperator::NOT_IN:
+                return true;
+        }
+        return false;
+    }
+}
