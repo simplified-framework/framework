8884412c5004a43ba3bf61825c7ec775ba5f3d17
Andreas Bratfisch <duskman72@gmail.com>
2016-01-01 22:31:07 +0100

    remove finder, it can be used through symfonie framework

--- core/src/ContainerInterface.php
+++ core/src/ContainerInterface.php
@@ -13,10 +13,6 @@ interface ContainerInterface {
      * Finds an entry of the container by its identifier and returns it.
      *
      * @param string $id Identifier of the entry to look for.
-     *
-     * @throws NotFoundExceptionInterface  No entry was found for this identifier.
-     * @throws ContainerExceptionInterface Error while retrieving the entry.
-     *
      * @return mixed Entry.
      */
     public function get($id);
